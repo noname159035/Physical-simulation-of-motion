dc4a466fc5f2b4048fc6bebae1dc8080fdbcd53b
Solovyev Maksim <maksim.solovev02@mail.ru>
2021-12-10 02:05:24 +0300

    added some new info

--- main.py
+++ main.py
@@ -1,3 +1,34 @@
+# Обозначение переменных
+#
+#движ по окружности
+# circle_U - скорость
+# circke_w - угловая сакорость
+# circke_R - радиус
+# circke_t - время
+# circke_S_way - путь
+# circke_a_o - нормальн ускорение
+#
+#линейное движ
+#
+# line_U_st - начальн скорость
+# line_a - ускорение
+# line_t - время
+# line_U_fin - кончен скорость
+# line_S_mov - перемещения
+# line_L - путь
+#
+#под углом
+#
+# angle_U_st - начальн скорость
+# angle_d - угол альфа
+# angle_t - время
+# angle_h_st - начальн высота
+# angle_U_fin - конечн скорость
+# angle_H - макс высота подъема
+# angle_L - длинна пути (горизонтальн)
+# angle_t_fall - время падения
+#
+
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow, QStackedWidget
 
