8409d351d610b7857f3979404cd17c8ce0883b93
OILaptin <oilaptin@edu.hse.ru>
2021-12-13 22:56:00 +0300

    образец написания count

--- count.py
+++ count.py
@@ -2,7 +2,6 @@ from metods import *
 def final_count_circle ():
     if metods.var['circle_t'] !=0 and :
 
-        metods.var['circle_t'] = metods.find_angle_H(metods.var['circle_w'], ..... аналогично заполнять )
+        #metods.var['circle_t'] = metods.find_angle_H(metods.var['circle_w'], ..... аналогично заполнять )
 
 
-def
