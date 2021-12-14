fa48a9a3dcb5f68cf4d94c842fbb5bb209ea0f6b
кирилл цыбочкин <kdtsybochkin@miem.hse.ru>
2021-12-14 14:18:01 +0300

    сделал под углом

--- count.py
+++ count.py
@@ -25,8 +25,15 @@ def final_count_angle ():
     metods.var["angle_U_st_x"] = metods.find_angle_U_st_x(metods.var['angle_U_st'], metods.var["angle_d"])
     metods.var["angle_U_st_y"] = metods.find_angle_U_st_y(metods.var['angle_U_st'], metods.var["angle_d"])
 
+    if metods.var["angle_t"] != 0:
+        metods.var["angle_t_fall"] = metods.find_angle_t_fall(metods.var["angle_U_st_y"])
+        metods.var["angle_H"] = metods.find_angle_H(metods.var["angle_h_st"], metods.var["angle_U_st_y"])
+        metods.var["angle_L"] = metods.find_angle_L(metods.var["angle_U_st_x"], metods.var["angle_t"])
 
+    elif metods.var["angle_H"] != 0 and metods.var["angle_t_fall"] !=0 and metods.var["angle_L"] != 0:
+        metods.var["angle_t"] = metods.find_angle_t(metods.var["angle_L"], metods.var["angle_U_st_x"])
+        metods.var["angle_h_st"] = metods.find_angle_h_st(metods.var["angle_t_fall"],metods.var["angle_H"])
 
-
-
+    if metods.var["angle_U_fin"] == 0:
+        metods.var["angle_U_fin"] = metods.find_angle_U_fin(metods.var["angle_h_st"], metods.var['angle_U_st'])
 
