19f4b678d24467be345df74e1b23f9fc9b7c6b5f
кирилл цыбочкин <kdtsybochkin@miem.hse.ru>
2021-12-14 13:26:59 +0300

    1/2 под углом

--- count.py
+++ count.py
@@ -17,6 +17,15 @@ def final_count_circle ():
         metods.var['line_S_mov'] = metods.find_line_S_mov2(metods.var['line_U_fin'], metods.var['line_a'],  metods.var['line_t'])
     metods.var['line_L'] = metods.find_line_L(metods.var['line_S_mov'], metods.var['line_t'], metods.var['line_a'])
 
+def final_count_angle ():
+    if metods.var["angle_U_fin"] != 0:
+        metods.var['angle_U_st'] = metods.find_angle_U_st(metods.var["angle_U_fin"], metods.var["angle_h_st"])
+    elif metods.var["angle_d"] == 0:
+        metods.var["angle_d"] =metods.find_angle_d(metods.var['angle_U_st'], metods.var['"angle_t_fall'])
+    metods.var["angle_U_st_x"] = metods.find_angle_U_st_x(metods.var['angle_U_st'], metods.var["angle_d"])
+    metods.var["angle_U_st_y"] = metods.find_angle_U_st_y(metods.var['angle_U_st'], metods.var["angle_d"])
+
+
 
 
 
