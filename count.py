1edc967d2dfc53a7b931b762b5aa638a89fa5508
OILaptin <90442905+OILaptin@users.noreply.github.com>
2021-12-16 15:03:23 +0300

    Движение по окр

--- count.py
+++ count.py
@@ -1,9 +1,8 @@
 import metods
 from metods import *
-def final_count_circle ():
-    if metods.var['circle_t'] !=0 and :
 
         #metods.var['circle_t'] = metods.find_angle_H(metods.var['circle_w'], ..... аналогично заполнять )
+def final_count_line():
     if metods.var['line_U_st'] != 0:
         metods.var['line_U_fin'] = metods.find_line_U_fin1(metods.var['line_a'], metods.var['line_t'], metods.var['line_U_st'])
         metods.var['line_S_mov'] = metods.find_line_S_mov1(metods.var['line_U_st'], metods.var['line_a'], metods.var['line_t'])
@@ -25,10 +24,8 @@ def final_count_circle():
             metods.var['circle_T'] = metods.find_circle_T(metods.var['circle_U'], metods.var['circle_R'])
             metods.var['circle_a_o'] = metods.find_circle_a_o(metods.var['circle_U'], metods.var['circle_R'])
             metods.var['circle_S'] = metods.find_circle_S(metods.var['circle_U'], metods.var['circle_t'])
-            metods.var['circle_delta_s'] = metods.find_circle_delta_s(metods.var['circle_S'], metods.var['circle_U'],
-                                                                      metods.var['circle_T'], metods.var['circle_t'])
-            metods.var['circle_delta_f'] = metods.find_circle_delta_f(metods.var['circle_delta_s'],
-                                                                      metods.var['circle_R'])
+            metods.var['circle_delta_s'] = metods.find_circle_delta_s(metods.var['circle_S'], metods.var['circle_U'], metods.var['circle_T'], metods.var['circle_t'])
+            metods.var['circle_delta_f'] = metods.find_circle_delta_f(metods.var['circle_delta_s'], metods.var['circle_R'])
 
 
     elif metods.var['circle_S'] != 0 and metods.var['circle_t'] != 0 and metods.var['circle_w'] != 0:
@@ -38,10 +35,8 @@ def final_count_circle():
             metods.var['circle_T'] = metods.find_circle_T(metods.var['circle_U'], metods.var['circle_R'])
             metods.var['circle_a_o'] = metods.find_circle_a_o(metods.var['circle_U'], metods.var['circle_R'])
             metods.var['circle_S'] = metods.find_circle_S(metods.var['circle_U'], metods.var['circle_t'])
-            metods.var['circle_delta_s'] = metods.find_circle_delta_s(metods.var['circle_S'], metods.var['circle_U'],
-                                                                      metods.var['circle_T'], metods.var['circle_t'])
-            metods.var['circle_delta_f'] = metods.find_circle_delta_f(metods.var['circle_delta_s'],
-                                                                      metods.var['circle_R'])
+            metods.var['circle_delta_s'] = metods.find_circle_delta_s(metods.var['circle_S'], metods.var['circle_U'], metods.var['circle_T'], metods.var['circle_t'])
+            metods.var['circle_delta_f'] = metods.find_circle_delta_f(metods.var['circle_delta_s'], metods.var['circle_R'])
 
     elif metods.var['circle_R'] != 0 and metods.var['circle_a_o'] != 0 and metods.var['circle_S'] != 0:
             metods.var['circle_U'] = metods.find_circle_U3(metods.var['circle_a_o'], metods.var['circle_R'])
