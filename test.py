2363eff10e9209ef6dc512fec8956e92a535b83b
кирилл цыбочкин <kdtsybochkin@miem.hse.ru>
2021-12-16 21:39:30 +0300

    тесты

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ class metod_test(unittest.TestCase):
     def test_5(self):
         self.assertEqual(find_circle_T(10, 2), 31.4)
     def test_6(self):
-        self.assertEqual(find_circle_delta_s(100, 5, ), 50)
+        self.assertEqual(find_circle_delta_s(100, 5, 2, 9), 50)
     def test_7(self):
         self.assertEqual(find_circle_a_o(5, 5), 5)
     def test_8(self):
