9b26cff239c02110fc839a3293e1ddb976794a73
кирилл цыбочкин <kdtsybochkin@miem.hse.ru>
2021-12-16 23:35:13 +0300

    тесты123

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import unittest
 from metods import *
+
 class metod_test(unittest.TestCase):
     def test_1(self):
         self.assertEqual(find_circle_w(10.4, 5.2), 2)
@@ -8,9 +9,9 @@ class metod_test(unittest.TestCase):
     def test_3(self):
         self.assertEqual(find_circle_a_o(5, 5), 5)
     def test_4(self):
-        self.assertEqual(find_circle_S(10, 5), 2)
+        self.assertEqual(find_circle_S(10, 5), 50)
     def test_5(self):
-        self.assertEqual(find_circle_T(10, 2), 31.4)
+        self.assertEqual(find_circle_T(10, 2), 1.256)
     def test_6(self):
         self.assertEqual(find_circle_delta_s(100, 5, 2, 9), 60)
     def test_7(self):
@@ -22,14 +23,15 @@ class metod_test(unittest.TestCase):
     def test_10(self):
         self.assertEqual(find_circle_R2(20, 5, 2), 2)
     def test_11(self):
-        self.assertEqual(find_circle_a_o(5, 5), 5)
+        self.assertEqual(find_circle_U3(20, 5), 10)
     def test_12(self):
-        self.assertEqual(find_circle_S(10, 5), 2)
+        self.assertEqual(find_circle_t(10, 2.5), 4)
+
     def test_13(self):
-        self.assertEqual(find_circle_T(10, 2), 31.4)
+        self.assertEqual(find_angle_U_st_x(10, 180), -10)
     def test_14(self):
-        self.assertEqual(find_circle_delta_s(100, 5, 2, 9), 60)
+        self.assertEqual(find_angle_U_st_x(10, 0), 10)
     def test_15(self):
-        self.assertEqual(find_circle_delta_f(65, 5), 13)
+        self.assertEqual(find_angle_t_fall(29.43), 3)
     def test_16(self):
-        self.assertEqual(find_circle_U2(15, 5), 3)
+        self.assertEqual(find_angle_H(10, 192.4722), 10)
