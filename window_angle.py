4c0a2c0b6c169cd7a35a5702d4d98c0fd840a870
кирилл цыбочкин <kdtsybochkin@miem.hse.ru>
2021-12-18 04:04:37 +0300

    комментарии

--- window_angle.py
+++ window_angle.py
@@ -22,6 +22,7 @@ class Ui_Angle(QMainWindow):
         self.button_angle_finish_input.clicked.connect(self.save_val)
 
     def save_speed_start(self):
+        '''Функция сохраняет начальную скорость в var'''
         try:
             var["angle_U_st"] = float(self.angle_input_speed_start.text())
             self.angle_input_speed_finish.hide()
@@ -31,6 +32,7 @@ class Ui_Angle(QMainWindow):
             print("[Log] Err: в ячейку подают строку")
 
     def save_angle(self):
+        '''Функция сохраняет угол, под которым тело начинает свое движение, в var'''
         try:
             var["angle_d"] = float(self.angle_input_angle.text())
             self.button_angle_input_time_fall.hide()
@@ -46,6 +48,7 @@ class Ui_Angle(QMainWindow):
             print("[Log] Err: в ячейку подают строку")
 
     def save_time(self):
+        '''Функция сохраняет время полета в var'''
         try:
             var["angle_t"] = float(self.angle_input_time.text())
             self.button_angle_input_time_fall.hide()
@@ -60,6 +63,7 @@ class Ui_Angle(QMainWindow):
             print("[Log] Err: в ячейку подают строку")
 
     def save_hight_start(self):
+        '''Функция сохраняет начальную в var'''
         try:
             var["angle_h_st"] = float(self.angle_input_hight_start.text())
             self.button_angle_input_time_fall.hide()
@@ -74,6 +78,7 @@ class Ui_Angle(QMainWindow):
             print("[Log] Err: в ячейку подают строку")
 
     def save_speed_finish(self):
+        '''Функция сохраняет конечную скорость в var'''
         try:
             var["angle_U_fin"] = float(self.angle_input_speed_finish.text())
 
@@ -92,6 +97,7 @@ class Ui_Angle(QMainWindow):
             print("[Log] Err: в ячейку подают строку")
 
     def save_bigHieght(self):
+        '''Функция сохраняет высоту подъема в var'''
         try:
             var["angle_H"] = float(self.angle_input_bigHieght.text())
 
@@ -110,6 +116,7 @@ class Ui_Angle(QMainWindow):
             print("[Log] Err: в ячейку подают строку")
 
     def save_length(self):
+        '''Функция сохраняет длину пути в var'''
         try:
             var["angle_L"] = float(self.angle_input_lenght.text())
             self.angle_input_speed_finish.hide()
@@ -129,6 +136,7 @@ class Ui_Angle(QMainWindow):
             print("[Log] Err: в ячейку подают строку")
 
     def save_time_fall(self):
+        '''Функция сохраняет время падения в var'''
         try:
             var["angle_t_fall"] = float(self.angle_input_time_fall.text())
             self.angle_input_speed_finish.hide()
@@ -147,6 +155,7 @@ class Ui_Angle(QMainWindow):
             print("[Log] Err: в ячейку подают строку")
 
     def save_val(self):
+        '''сохраняет все значения, запускает метод подсчёта, переходит на окно вывода расчётов'''
         final_count_angle()
         uic.loadUi('Forms\output_angle.ui', self)
         self.angle_out_speed_start.insert(str(var["angle_U_st"]))
@@ -161,6 +170,7 @@ class Ui_Angle(QMainWindow):
         self.angle_show_graph.clicked.connect(self.get_graph)
 
     def get_graph(self):
+        """генерирует и выводить график"""
         t = 0
         x, y = [], []
         while t < var["angle_t"]:
